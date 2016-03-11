a7096a88a133cf91ef96eafd876ab9c55bb3b557
NL-BlackDragon <blackdragon.vrtoxin@gmail.com>
2016-03-11 15:02:17 +0100

    Merge tag 'android-6.0.1_r22'
    Android 6.0.1 release 22
    
    Signed-off-by: NL-BlackDragon <blackdragon.vrtoxin@gmail.com>

--- src/com/android/server/telecom/ui/MissedCallNotifierImpl.java
+++ src/com/android/server/telecom/ui/MissedCallNotifierImpl.java
@@ -16,6 +16,8 @@
 
 package com.android.server.telecom.ui;
 
+import android.content.ComponentName;
+import android.telecom.TelecomManager;
 import com.android.server.telecom.Call;
 import com.android.server.telecom.CallState;
 import com.android.server.telecom.CallerInfoAsyncQueryFactory;
@@ -63,6 +65,8 @@ import android.text.format.DateUtils;
 import android.text.style.RelativeSizeSpan;
 
 import java.util.ArrayList;
+import java.lang.Override;
+import java.lang.String;
 import java.util.Locale;
 
 // TODO: Needed for move to system service: import com.android.internal.R;
@@ -117,11 +121,16 @@ public class MissedCallNotifierImpl extends CallsManagerListenerBase implements 
         }
     };
     private ArrayList<MissedCallInfo> mMissedCalls = new ArrayList<MissedCallInfo>();
+    private final ComponentName mNotificationComponent;
 
     public MissedCallNotifierImpl(Context context) {
         mContext = context;
         mNotificationManager =
                 (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+        final String notificationComponent = context.getString(R.string.notification_component);
+
+        mNotificationComponent = notificationComponent != null
+                ? ComponentName.unflattenFromString(notificationComponent) : null;
     }
 
     /** {@inheritDoc} */
@@ -158,6 +167,44 @@ public class MissedCallNotifierImpl extends CallsManagerListenerBase implements 
             }
         });
         cancelMissedCallNotification();
+    }
+
+    /**
+     * Broadcasts missed call notification to custom component if set.
+     * @param number The phone number associated with the notification. null if
+     *               no call.
+     * @param count The number of calls associated with the notification.
+     * @return {@code true} if the broadcast was sent. {@code false} otherwise.
+     */
+    private boolean sendNotificationCustomComponent(Call call, int count) {
+        if (mNotificationComponent != null) {
+            Intent intent = new Intent();
+            intent.setFlags(Intent.FLAG_RECEIVER_FOREGROUND);
+            intent.setComponent(mNotificationComponent);
+            intent.setAction(TelecomManager.ACTION_SHOW_MISSED_CALLS_NOTIFICATION);
+            intent.putExtra(TelecomManager.EXTRA_NOTIFICATION_COUNT, count);
+            intent.putExtra(TelecomManager.EXTRA_NOTIFICATION_PHONE_NUMBER,
+                    call != null ? call.getPhoneNumber() : null);
+            intent.putExtra(TelecomManager.EXTRA_CLEAR_MISSED_CALLS_INTENT,
+                    createClearMissedCallsPendingIntent());
+
+
+            if (count == 1 && call != null) {
+                final Uri handleUri = call.getHandle();
+                String handle = handleUri == null ? null : handleUri.getSchemeSpecificPart();
+
+                if (!TextUtils.isEmpty(handle) && !TextUtils.equals(handle,
+                        mContext.getString(R.string.handle_restricted))) {
+                    intent.putExtra(TelecomManager.EXTRA_CALL_BACK_INTENT,
+                            createCallBackPendingIntent(handleUri));
+                }
+            }
+
+            mContext.sendBroadcast(intent);
+            return true;
+        }
+
+        return false;
     }
 
     /**
